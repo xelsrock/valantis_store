b4d2755c8b280ef90df8bed6bfe26295fc03ca03
xelsrock <alex.perevosnicov@gmail.com>
2024-02-29 21:03:02 +0300

    get product

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import md5 from 'md5';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const xAuth = md5('Valantis_20240229');
+
+const App = () => {
+  const [product, setProduct] = useState([]);
+
+  const noDupl = (array) => {
+    return array.filter((val, idx, arr) => arr.findIndex((val2) => val2.id === val.id) === idx);
+  };
+
+  const fetchProduct = async () => {
+    try {
+      const responseId = await fetch('http://api.valantis.store:40000/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Auth': xAuth,
+        },
+        body: JSON.stringify({ action: 'get_ids', params: { offset: 0, limit: 50 } }),
+      }).then((res) => res.json());
+
+      await fetch('http://api.valantis.store:40000/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Auth': xAuth,
+        },
+        body: JSON.stringify({
+          action: 'get_items',
+          params: { ids: responseId.result },
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => setProduct(noDupl(data.result)));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProduct();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {product.map((item) => (
+        <div key={item.id}>{item.id}</div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
